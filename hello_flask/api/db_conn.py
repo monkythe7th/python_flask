399280ee04f3fdc9e49ae20191579c87140e0d0b
Naleli Ramoeletsi <monky.the7th@gmail.com>
2023-02-23 22:54:48 +0200

    Update db_conn.py
    obfiscation

--- hello_flask/api/db_conn.py
+++ hello_flask/api/db_conn.py
@@ -1,10 +1,10 @@
 from pymongo import MongoClient
 import pymongo
 
-uri = "mongodb+srv://cluster0.7nocv.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
+uri = "test server connection url"
 client = MongoClient(uri,
                     tls=True,
-                    tlsCertificateKeyFile='./api/X509-cert-5642347178223945930.pem')
+                    tlsCertificateKeyFile='./api/X509-cert.pem')
 db = client["helloFlask"]
 
 def get_col(col):
